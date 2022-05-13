04ac76903cb8dde2009222bdf0b0988e4daed016
Niraj Hirpara <nirajhirapara956@gmail.com>
2022-05-14 00:30:14 +0530

    Login and register done with database

--- app/src/main/java/activities/LoginActivity.java
+++ app/src/main/java/activities/LoginActivity.java
@@ -1,27 +1,88 @@
 package activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.studreference.MainActivity;
 import com.example.studreference.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://studreference-abfb4-default-rtdb.firebaseio.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        final EditText mobileNo = findViewById(R.id.mobile);
+        final EditText password = findViewById(R.id.password);
+
+        final Button registerBtn2 = findViewById(R.id.registerbtn2);
+        final Button loginBtn1 = findViewById(R.id.loginbtn1);
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
-        Button signupButton = findViewById(R.id.signup_in_login_page);
-        signupButton.setOnClickListener(new View.OnClickListener() {
+        loginBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String mobileTxt = mobileNo.getText().toString();
+                final String passwordTxt = password.getText().toString();
+
+                if(mobileTxt.isEmpty() || passwordTxt.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please enter your mobile or password", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(mobileTxt)) {
+                                //get password from firebase database of users using mobileText
+                                final String getPassword = snapshot.child(mobileTxt).child("password").getValue(String.class);
+
+                                if(getPassword.equals(passwordTxt)) {
+                                    Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+
+                                    //go to home page on success
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                                else {
+                                    Toast.makeText(LoginActivity.this, "Wrong password!!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+
+        registerBtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
@@ -29,13 +90,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button loginButton = findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
--- app/src/main/java/activities/SignupActivity.java
+++ app/src/main/java/activities/SignupActivity.java
@@ -1,27 +1,96 @@
 package activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.studreference.MainActivity;
 import com.example.studreference.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
+
+    //create object of database reference class to access firebase realtime database
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://studreference-abfb4-default-rtdb.firebaseio.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        final EditText fullName = findViewById(R.id.fullname);
+        final EditText mobileNo = findViewById(R.id.mobile);
+        final EditText emailId = findViewById(R.id.email);
+        final EditText collegeName = findViewById(R.id.college);
+        final EditText branchName = findViewById(R.id.branch);
+        final EditText password = findViewById(R.id.password);
+
+        final Button registerBtn1 = findViewById(R.id.registerbtn1);
+        final Button loginBtn2 = findViewById(R.id.loginbtn2);
+
+        //this will check letter
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
-        Button loginButton = findViewById(R.id.login_btn_in_signup_page);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        registerBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //get data from edit text into string variable
+                final String fullnameTxt = fullName.getText().toString();
+                final String mobileTxt = mobileNo.getText().toString();
+                final String emailTxt = emailId.getText().toString();
+                final String collegeTxt = collegeName.getText().toString();
+                final String branchTxt = branchName.getText().toString();
+                final String passwordTxt = password.getText().toString();
+
+                if(fullnameTxt.isEmpty() || mobileTxt.isEmpty() || emailTxt.isEmpty() || collegeTxt.isEmpty() || branchTxt.isEmpty() || passwordTxt.isEmpty()) {
+                    Toast.makeText(SignupActivity.this, "please fill all the fields", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(mobileTxt)) {
+                                Toast.makeText(SignupActivity.this, "Phone number is already registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                //sending data to firebase realtime database
+                                //mobile number is unique id of every user
+                                databaseReference.child("users").child(mobileTxt).child("fullname").setValue(fullnameTxt);
+                                databaseReference.child("users").child(mobileTxt).child("email").setValue(emailTxt);
+                                databaseReference.child("users").child(mobileTxt).child("college").setValue(collegeTxt);
+                                databaseReference.child("users").child(mobileTxt).child("Branch").setValue(branchTxt);
+                                databaseReference.child("users").child(mobileTxt).child("password").setValue(passwordTxt);
+
+                                Toast.makeText(SignupActivity.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+        loginBtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
@@ -29,13 +98,6 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-        Button signupButton = findViewById(R.id.signup_btn);
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
